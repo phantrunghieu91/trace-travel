3ab8e015291576d1649029906c78cd83aa66de1c
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-05-20 16:22:56 +0700

    UPDATE: Home page - Featured trips: Added display_style options + style

--- shortcodes/homepage_featured_products_new.php
+++ shortcodes/homepage_featured_products_new.php
@@ -2,30 +2,43 @@
 add_shortcode('homepage_featured_products', function ( $atts ) {
   extract( shortcode_atts( [
     'prd_cat_id' => 0,
+    'display_style' => 'carousel' // carousel || grid
   ], $atts ));
   $products = wc_get_products([
     'status' => 'publish',
-    'limit' => 6,
+    'limit' => $display_style === 'carousel' ? 6 : 8,
     'product_category_id' => [ $prd_cat_id ],
   ]);
+  $classes = [ 'featured-trips' ];
+  $classes[] = $display_style === 'carousel' ? 'featured-trips--carousel' : 'featured-trips--grid';
   ob_start();
   ?>
-  <div class="featured-trips">
-    <div class="swiper">
-      <div class="swiper-wrapper">
-        <?php foreach( $products as $product ) {
-          unset($GLOBALS['product']);
-          $GLOBALS['product'] = $product;
-          echo '<div class="swiper-slide">';
-          get_template_part( 'gpw-templates/woocommerce/product-card' );
-          echo '</div>';
-        } ?>
+  <div class="<?= esc_attr( implode( ' ', $classes )) ?>">
+    <?php if( $display_style === 'carousel' ) : ?>
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <?php foreach( $products as $product ) {
+            unset($GLOBALS['product']);
+            $GLOBALS['product'] = $product;
+            echo '<div class="swiper-slide">';
+            get_template_part( 'gpw-templates/woocommerce/product-card' );
+            echo '</div>';
+          } ?>
+        </div>
+        <a href="javascript:void(0);" role="button" class="swiper-button-prev" aria-label="Previous Product">
+        </a>
+        <a href="javascript:void(0);" role="button" class="swiper-button-next" aria-label="Next Product">
+        </a>
       </div>
-      <a href="javascript:void(0);" role="button" class="swiper-button-prev" aria-label="Previous Product">
-      </a>
-      <a href="javascript:void(0);" role="button" class="swiper-button-next" aria-label="Next Product">
-      </a>
-    </div>
+    <?php else: ?>
+
+      <?php foreach( $products as $product ) {
+        unset($GLOBALS['product']);
+        $GLOBALS['product'] = $product;
+        get_template_part( 'gpw-templates/woocommerce/product-card' );
+      } ?>
+
+    <?php endif ?>
     
   </div><!-- Close featured-trips -->
   <?php
